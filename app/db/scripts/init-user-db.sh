0ccf4a2c2a55ca51b0819c23d543f1d438e2e47a
Matthias Christen <chrismat@students.zhaw.ch>
2022-01-17 10:59:24 +0100

    Removed sunspotter-app from this repository

--- app/db/scripts/init-user-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER sunspotterdb;
-    GRANT ALL PRIVILEGES ON DATABASE sunspotter TO sunspotterdb;
-EOSQL
